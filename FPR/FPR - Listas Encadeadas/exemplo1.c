0ba144e1e9faa81c8d0706448a1a0bfd62345ac6
hyperGT <charlesgabriel102@gmail.com>
2024-11-07 08:56:23 -0300

    lista encadeada simples: inserir e exibir implementados

--- FPR/FPR - Listas Encadeadas/exemplo1.c
+++ FPR/FPR - Listas Encadeadas/exemplo1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // definindo tipos
 typedef struct No{
@@ -8,9 +9,58 @@ typedef struct No{
 
 }TNo;
 
+typedef TNo* Tlista;
+
 // funções
-int inserir(Tno **L);
+int inserir(Tlista *L, int valor);
+void exibir(Tlista L);
 
 void main(){
 
+    Tlista lista =  NULL;    
+
+    /* MENU */
+    inserir(&lista, 10);
+    inserir(&lista, 20);
+
+    exibir(lista);
+
+}
+
+int inserir(Tlista *L, int valor){
+
+    Tlista aux;
+
+    // alocar memoria pro novo nó
+    aux = malloc(sizeof(TNo));
+
+    if(!aux){
+        return 0;
+    }else{
+        
+        aux->valor = valor;     // Atribui o valor ao nó
+        
+        aux->prox = *L;         // O ponteiro do novo nó aponta para o primeiro nó da lista
+
+        *L = aux;               // Atualiza o ponteiro da lista para o novo nó (agora ele é o primeiro)
+
+        return 1;
+    }
+
+
+}
+
+void exibir(Tlista L){
+
+    Tlista aux = L;
+
+    if(aux == NULL){
+        printf("Lista encadeada está vazia");
+    }
+
+    while(aux != NULL){
+        printf("%d, ", aux->valor);
+        aux = aux->prox;
+    }
+    printf("\n");
 }
